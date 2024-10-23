93ba8829a87fa2b43f7d6ea819ecb8a4f902cab3
CiaranEnglish <ciaran.english.2020@mumail.ie>
2024-10-23 18:51:29 +0100

    Addition of react to projct

--- Weather_App/Backend/script.py
+++ Weather_App/Backend/script.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/weather', methods=['GET'])
 def get_weather():
     city = request.args.get('city')
-    api_key = 'YOUR_API_KEY'
+    api_key = '873dbf28df7658a02a6bc76a3e948efa '
     url = f'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={api_key}'
     response = requests.get(url)
     data = response.json()
